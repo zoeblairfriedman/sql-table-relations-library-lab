9a34e74e892587b2f52c2c0fa931eafeb6b52b4f
Liz Burton <liz.burton147@gmail.com>
2020-10-23 14:17:57 -0400

    Fixes failing solution code

--- lib/querying.rb
+++ lib/querying.rb
@@ -61,6 +61,6 @@ def select_character_names_and_number_of_books_they_are_in
     JOIN characters 
     ON character_books.character_id = characters.id 
     GROUP BY characters.name 
-    ORDER BY book_count DESC
+    ORDER BY book_count DESC, characters.name
   SQL
 end
